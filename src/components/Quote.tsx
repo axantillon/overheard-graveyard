d13495e068fa08f1a2586c94368cb62143208a9f
axantillon <aantillon.leiva@gmail.com>
2022-10-25 20:57:23 -0600

    fix spacing in quote

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -8,15 +8,15 @@ export const Quote: FC<{quote: QuoteType}> = ({quote}) => {
 
     return (
         <div className="flex flex-col w-full px-8 py-4 space-y-6 bg-white rounded-xl">
-            <div className="flex items-start justify-between">
+            <div className="flex items-start justify-between w-full">
                 <span className="text-xxs md:text-xs text-stone-400 italic"><b>@{quote.author}</b> overheard</span>
                 <span className="text-xxs md:text-xs text-stone-400">{quote.date_created}</span>
             </div>
-            <div className="flex items-center min-h-[4rem]">
+            <div className="flex items-center min-h-[4rem] w-full">
                 <div className="relative flex items-center justify-center h-10 md:h-12 w-10 md:w-12">
                     <Image onError={() => setSrc("/default_user.png")} className="rounded-full h-12 w-12" src={src} fill object-fit="cover" alt=''/>
                 </div>
-                <div className="flex flex-col justify-around w-full px-6 space-y-2">
+                <div className="flex flex-col justify-around w-full pr-6 space-y-2">
                     <span className="text-xs md:text-base font-semibold">{`"${quote.content}"`}</span>
                     <span className="text-xs md:text-base text-stone-400">{`-${quote.signature} @${quote.quotee}`}</span>
                 </div>
